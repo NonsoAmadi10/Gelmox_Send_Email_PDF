1668eb77154e49784b1f26b290fb3d53d880e5d4
Houdini <nonsoamadi@aol.com>
2020-07-27 16:52:41 +0100

    switch to js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 
+import * as pdfConverter from "html-pdf-node";
+
 import * as dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
-
 const pathToHtml = path.resolve(path.join(__dirname, "test.html"));
+const myHtml = fs.readFileSync(pathToHtml, "utf8");
 
-console.log(fs.readFileSync(pathToHtml, "utf8"));
 const app = express();
 app.use(express.json());
 app.use(cors());
