868c5a11c7d598d496b3ea9665bd01806eed6a3c
Houdini <nonsoamadi@aol.com>
2020-07-30 00:57:07 +0100

    set urlencoded true

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send({
     "message": "Hello there! Send an Order to gelmox logistics! Contact Nonso Amadi for guide"
