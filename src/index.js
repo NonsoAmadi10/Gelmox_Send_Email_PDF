4aed24f10d86d037375355dc7a232c5a08fe088c
Houdini <nonsoamadi@aol.com>
2020-07-30 00:50:33 +0100

    debug -2

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ import '@babel/polyfill';
 import sanitizer from './utils/middleware';
 
 const app = express();
-app.use(express.json());
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send({
     "message": "Hello there! Send an Order to gelmox logistics! Contact Nonso Amadi for guide"
